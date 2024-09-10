2541ae3ea93eea3925eebd66046060ab6ac802e8
Nicolás Quezada Baier <david.avila@uach.cl>
2024-09-09 23:10:33 -0300

    nueva versión

--- screens/help_screen.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class HelpScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        print("HelpScreen initialized")
--- utils/theme.py
+++
@@ -1,4 +0,0 @@
-def set_theme(app):
-    app.theme_cls.primary_palette = 'Beige'
-    app.theme_cls.theme_style = 'Light'  # Puedes cambiar a 'Dark' si prefieres el tema oscuro
-    print(f"Theme set to {app.theme_cls.primary_palette} with style {app.theme_cls.theme_style}")
